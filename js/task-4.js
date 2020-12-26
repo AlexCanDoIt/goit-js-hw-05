30ef03889eec783c722cac604220dd5fc96ac253
AlexCanDoIt <alexcandoitt@gmail.com>
2020-12-26 19:55:18 +0200

    Update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -8,15 +8,16 @@ class StringBuilder {
   }
 
   append(str) {
-    return (this._value += str);
+    this._value += str;
   }
 
   prepend(str) {
-    return (this._value = str + this._value);
+    this._value = str + this._value;
   }
 
   pad(str) {
-    return (this._value = str + this._value + str);
+    this.append(str);
+    this.prepend(str);
   }
 }
 
